dbf3211e191926a6407c8487223c1991a713b221
Dansky <dansky123@hotmail.com>
2022-07-05 21:06:51 -0300

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    protected DataContext()
+    {
+    }
+
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+
+    public DbSet<AppUser> Users {get; set;}
+}
